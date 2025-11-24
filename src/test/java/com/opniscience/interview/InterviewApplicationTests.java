d10957365afdc6d72c25693eed72f58a9c6b6fc1
Evan Steinkerchner <esteinkerchner@gmail.com>
2025-11-24 15:04:22 -0500

    Initial commit

---
+++ src/test/java/com/opniscience/interview/InterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opniscience.interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
